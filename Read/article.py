b3c199b4c9f0503638e765cde03edf697e85d6b6
Shengyang Shi <shengyang.shi@hotmail.com>
2015-11-29 21:13:45 -0800

    optimization

--- Read/article.py
+++ Read/article.py
@@ -1,10 +1,7 @@
 import numpy as np
-
-class Article:
     
-    # Transform a single char to a 256 length binary vector 
-    @staticmethod
-    def tc2v(c):
-        v = np.zeros(256, dtype = np.int8)
-        v[ord(c)] = 1
-        return v
+# Transform a single char to a 256 length binary vector 
+def tc2v(c):
+    v = np.zeros(256, dtype = np.int8)
+    v[ord(c)] = 1
+    return v
