b4525f066b2fa198d24045c25fab65ba9d064f28
coolmich <coolmich00@gmail.com>
2015-12-01 10:36:29 -0800

    remove pyc, add library setup

---
+++ rnn/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name='rnn',
+        version='0.0.0',
+        author='Jacob Tao',
+        author_email='coolmich00@gmail.com',
+        description='recurrent neural network hw4',
+        classifiers=['Private :: Do Not Upload'],
+        install_requires=['numpy'],
+        url='',
+        packages=[]
+        )
